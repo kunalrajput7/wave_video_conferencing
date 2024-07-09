f5b99394d52e7a20b74f23112fce2bcc7a1207af
Kunal Rajput <kunalrajput15112000@gmail.com>
2024-07-09 02:03:45 -0500

    Meet Scheduling fully functional!

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -9,25 +9,25 @@ export const metadata: Metadata = {
   icons: 'icons/wave-logo.png'
 };
 
-const RootLayout = ({ children }: {children: ReactNode}) => {
+const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main className='relative'>
-        <Navbar/>
+      <Navbar />
 
-        <div className='flex'>
-            <Sidebar/>
+      <div className='flex'>
+        <Sidebar />
 
-            <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
 
-                <div>
+          <div>
 
-                    {children}
+            {children}
 
-                </div>
+          </div>
 
-            </section>
-            
-        </div>
+        </section>
+
+      </div>
 
 
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import { Button } from '@/components/ui/button';
 import { ButtonProps } from '../components/ui/button';
 import { Toaster } from "@/components/ui/toaster";
 import "@stream-io/video-react-sdk/dist/css/styles.css";
+import 'react-datepicker/dist/react-datepicker.css';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
